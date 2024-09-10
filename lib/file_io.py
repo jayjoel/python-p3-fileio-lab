8a947bc73d72bea50f6f5ce1e650c412088cea3d
joel_patrick <jayjoelpn@gmail.com>
2024-09-10 14:07:14 +0300

    initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w") as file:
+        file.write(file_content)    
+        
+file_content = 'This is a test content.'
+file_name = "text_name.txt"
+
+write_file(file_name, file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r") as file:
+        return file.read()
